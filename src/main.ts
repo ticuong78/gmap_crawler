5fd965c4696249cfa0220f2c97b4a3f004f6984f
Cường Lê <cuongdayne17@gmail.com>
2026-04-06 06:47:02 +0700

    done

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { Queue } from "@src/1_application/data_structure/Queue";
 import { CollectPlaceUrlUseCase } from "@src/1_application/usecases/CollectPlaceUrlUseCase";
 import { ConsoleLogger } from "@src/2_infrastructure/loggers/ConsoleLogger";
 import { PuppeteerPageHandle } from "@src/2_infrastructure/puppeteer/PuppeteerPageHandle";
+import * as pie from "puppeteer-in-electron";
+import { app, BrowserWindow } from "electron";
+import * as puppeteer from "puppeteer-core";
 
 export type MainComposition = {
   logger: ConsoleLogger;
@@ -10,6 +13,21 @@ export type MainComposition = {
   urlQueue: Queue<string>;
   collectPlaceUrlUseCase: CollectPlaceUrlUseCase;
 };
+
+export async function createPIE() {
+  // Initialize puppeteer-in-electron
+  await pie.initialize(app);
+
+  // Connect Puppeteer to Electron
+  const browser = await pie.connect(app, puppeteer);
+
+  // Create a new Electron window
+  const window = new BrowserWindow();
+
+  const page = await pie.getPage(browser, window);
+
+  return page;
+}
 
 export function createMainComposition(page: Page): MainComposition {
   const logger = new ConsoleLogger();
@@ -28,3 +46,10 @@ export function createMainComposition(page: Page): MainComposition {
     collectPlaceUrlUseCase,
   };
 }
+
+const page = await createPIE();
+
+const { logger, pageHandle, urlQueue, collectPlaceUrlUseCase } =
+  createMainComposition(page);
+
+collectPlaceUrlUseCase.execute();
